df9054919dd8705ac7a7a7abbbdb9eaeac3b9efb
fraimparato <fra.imparato20@gmail.com>
2023-06-28 15:32:40 +0200

    add: 2023-06-27 test database and related csv

--- Phoneix Data/database/read_databases.py
+++ Phoneix Data/database/read_databases.py
@@ -1,11 +1,11 @@
 import sqlite3
 import pandas as pd
 
-con = sqlite3.connect('database.db')
+con = sqlite3.connect('2023-06-27.db')
 
 data = pd.read_sql_query('Select * from ant', con)
 
 # print(data.head())
-data.to_csv('franck.csv', index=False)
+data.to_csv('2023-06-27.csv', index=False)
 
 
