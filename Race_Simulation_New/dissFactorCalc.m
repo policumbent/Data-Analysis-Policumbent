c4f783d4d2affc9ece8c7a8266e939e16b8fcadc
Lorenzo Villani <villani743@gmail.com>
2023-07-30 18:00:17 +0200

    Improved the usability of the code by adding sliders. Now is possible to take in account some differences between cerberus and phoenix.
     Changes to be committed:
    	modified:   Race_Simulation_New/RaceSim.mlx
    	modified:   Race_Simulation_New/dissFactorCalc.m

--- Race_Simulation_New/dissFactorCalc.m
+++ Race_Simulation_New/dissFactorCalc.m
@@ -1,10 +1,11 @@
-function [Lambda, cal] = dissFactorCalc(RPM_wheel_bo_RPM_pedal,vel_lin_bo_RPM,IdRun)
+function [Lambda, cal] = dissFactorCalc(RPM_wheel_bo_RPM_pedal,vel_lin_bo_RPM,IdRun,bike)
 
     [~,~,~,~,~,m,~,radius,ID] = readCond(IdRun);
     [~,~,vel_lin,power,dist,~,~,~] = readRace(ID);
     
     % Variables pre-allocation
     L = size(RPM_wheel_bo_RPM_pedal,1)-1;
+    wheels = 0;
     Jp = zeros(L);          % Energy calculated from linear velocity of the bike
     Jt = zeros(L);          % Energy calculated from RPM of the wheel
     delta_x = zeros(L);     % Difference between two subsequent data [Km]
@@ -12,11 +13,20 @@ function [Lambda, cal] = dissFactorCalc(RPM_wheel_bo_RPM_pedal,vel_lin_bo_RPM,Id
     Ker_t = zeros(L);       % Rotational kinetic energy calculated from RPM of the wheel
     I = 0.044;              % Inertia of the wheel
 
+    if(strcmp(bike,"cerberus") == 1)
+        wheels = 3;
+    elseif(strcmp(bike,"phoenix") == 1)
+        wheels = 2;
+    else
+        disp("Not a valide choiche for the bike");
+        exit(1);
+    end
+
     for i = 1:L
         Jp(i) = 0.5 * m *(vel_lin(i+1)^2 + vel_lin(i)^2)/2;
         Jt(i) = 0.5 * m * (vel_lin_bo_RPM(i+1)^2 + vel_lin_bo_RPM(i)^2)/2;
-        Ker_p(i) = I * ((vel_lin(i+1) / radius)^2 + (vel_lin(i) / radius)^2)/2;
-        Ker_t(i) = I * ((vel_lin_bo_RPM(i+1) / radius)^2 + (vel_lin_bo_RPM(i) / radius)^2)/2;
+        Ker_p(i) = 0.5 * I * wheels * ((vel_lin(i+1) / radius)^2 + (vel_lin(i) / radius)^2)/2;
+        Ker_t(i) = 0.5 * I * wheels* ((vel_lin_bo_RPM(i+1) / radius)^2 + (vel_lin_bo_RPM(i) / radius)^2)/2;
         delta_x(i) = dist(i+1) - dist(i);
     end
 
