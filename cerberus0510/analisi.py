70d6c5657b6d56c8dc8ed6544aeda77b6cfcda6e
Mario <mr.tarantino02@gmail.com>
2025-05-14 13:02:17 +0200

    creazione mappa

--- cerberus0510/analisi.py
+++ cerberus0510/analisi.py
@@ -2,13 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from matplotlib.collections import LineCollection
+import matplotlib.colors as mcolors
+import matplotlib.image as mpimg
+import folium
 
 
 df = pd.read_csv('giro1.csv')
 
+
 # test 1 tutti i dati buttati iniseme 
 
-plt.figure(figsize=(10, 8))
+'''plt.figure(figsize=(10, 8))
 sc = plt.scatter(df['longitudine'], df['latitudine'], c=df['velocità'], cmap='viridis', s=10)
 plt.colorbar(sc, label='Velocità (km/h)')
 plt.xlabel('Longitudine')
@@ -16,48 +21,12 @@ plt.ylabel('Latitudine')
 plt.title('Tracciato geografico colorato per velocità')
 plt.grid(True)
 plt.axis('equal')
-plt.show()
+plt.show()'''
 
 
 # test 2 con creazione animata del percorso
 
-'''df = df.sort_values(by='tempo').reset_index(drop=True)
-
-# Setup figura
-fig, ax = plt.subplots(figsize=(10, 8))
-ax.set_xlim(df['longitudine'].min(), df['longitudine'].max())
-ax.set_ylim(df['latitudine'].min(), df['latitudine'].max())
-ax.set_xlabel('Longitudine')
-ax.set_ylabel('Latitudine')
-ax.set_title('Animazione del percorso nel tempo')
-ax.set_aspect('equal')
-line, = ax.plot([], [], lw=2, color='blue')  # linea animata
-
-# Funzione di inizializzazione
-def init():
-    line.set_data([], [])
-    return line,
-
-# Funzione che aggiorna il grafico per ogni frame
-def update(frame):
-    x = df['longitudine'].iloc[:frame]
-    y = df['latitudine'].iloc[:frame]
-    line.set_data(x, y)
-    return line,
-
-# Crea animazione
-ani = FuncAnimation(fig, update, frames=len(df), init_func=init, interval=10, blit=True)
-
-plt.show()'''
-
-'''import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.collections import LineCollection
-from matplotlib.animation import FuncAnimation
-import matplotlib.colors as mcolors
-import matplotlib.image as mpimg
-
-
+'''
 x = df['longitudine'].values
 y = df['latitudine'].values
 speed = df['velocità'].values
@@ -104,4 +73,13 @@ cbar.set_label('Velocità (km/h)')
 ani = FuncAnimation(fig, update, frames=len(segments), interval=30, blit=True)
 plt.show()'''
 
+
+# test 3 creazione mappa
+
+mappa = folium.Map(location=[df['latitudine'][0], df['longitudine'][0]], zoom_start=14)
+
+percorso = list(zip(df['latitudine'], df['longitudine']))
+folium.PolyLine(locations=percorso, color="blue", weight=2.5, opacity=1).add_to(mappa)
+
+mappa.save("percorso_bici.html")
 
