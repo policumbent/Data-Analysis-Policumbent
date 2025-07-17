ecb4b045a1738d5dde2597ba46cd67acdafcd446
Mario <mr.tarantino02@gmail.com>
2025-07-17 20:20:14 +0200

    modifiche

--- dataDistiller/plottiamo/testCerberus.py
+++ dataDistiller/plottiamo/testCerberus.py
@@ -1,46 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import pi
 
 
-file1 = '/Users/mario/Data-Analysis-Policumbent/dati/cerberus/balocco/20250614/run2/pwmR1.csv'   
-file2 = '/Users/mario/Data-Analysis-Policumbent/dati/cerberus/balocco/20250614/run2/hearthrateR1.csv'      
-
-colonne_da_plottare_file1 = ['instant_power'] 
-colonne_da_plottare_file2 = ['avg_heartrate']
-
-
-label_file1 = "Power"
-label_file2 = "Heartrate"
-
-
+file1 = '../../dati/cerberus/balocco/20250614/run1/run4.csv'        
 
 df1 = pd.read_csv(file1)
-df2 = pd.read_csv(file2)
 
-df1['coppia'] = df1['power']/(df1['cadence']*(2*3.14/60))
+df1['coppia'] = df1['power']/(df1['cadence']*(2*pi/60))
 
 df1['timestamp'] = pd.to_datetime(df1['timestamp'], errors='coerce')
-df2['timestamp'] = pd.to_datetime(df2['timestamp'], errors='coerce')
-
-
 
 plt.figure(figsize=(12, 6))
 
 
-for col in colonne_da_plottare_file1:
-    plt.plot(df1['timestamp'], df1[col], label=f"{label_file1}: {col}")
-
-
-for col in colonne_da_plottare_file2:
-    plt.plot(df2['timestamp'], df2[col], label=f"{label_file2}: {col}")
-
-
-for col in ['coppia']:
-    plt.plot(df1['timestamp'], df1[col], label=f"coppia : {col}")
-
-
 for col in ['cadence']:
-    plt.plot(df1['timestamp'], df1[col], label=f'cadenza')
+    plt.plot(df1['timestamp'], df1[col], label=f"cadenza : {col}")
+
+
 
 plt.xlabel("Tempo")
 plt.ylabel("Valori")
@@ -49,3 +26,5 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+#rifare il file run1.csv in run1
--- dataDistiller/pulizia/filtroFrequenza.py
+++ dataDistiller/pulizia/filtroFrequenza.py
@@ -1,11 +1,7 @@
 import pandas as pd
 
 
-def hearthrate():
-    inpF = '../../dati/cerberus/balocco/20250614/rowdata/run1/hearthrate_13-06-2025@16_50_55.csv'
-    outF = '../../dati/cerberus/balocco/20250614/run1/hearthrateR1.csv'
-
-
+def hearthrate(inpF, outF):
     df = pd.read_csv(inpF)
 
     # Converte la colonna timestamp in datetime e tronca ai secondi
@@ -22,13 +18,8 @@ def hearthrate():
     print(f"Salvato: {outF}")
 
 
-def powermeter():
-    inpF = '../../dati/cerberus/balocco/20250614/rowdata/run1/powermeter_13-06-2025@16_50_55.csv'
-    outF = '../../dati/cerberus/balocco/20250614/run1/pwmR1.csv'
-
-
+def powermeter(inpF, outF):
     df = pd.read_csv(inpF)
-
     
     df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
     df = df.dropna(subset=['timestamp'])  # rimuove righe con timestamp non validi
@@ -43,13 +34,39 @@ def powermeter():
     print(f"Salvato: {outF}")
 
 
+def unifica(inpF, inpF2, outF):
+    df1 = pd.read_csv(inpF)
+    df2 = pd.read_csv(inpF2)
+
+    df1['timestamp'] = pd.to_datetime(df1['timestamp'], errors='coerce')
+    df2['timestamp'] = pd.to_datetime(df2['timestamp'], errors='coerce')
+
+
+    #seleziono colonne da unire
+
+    df1_sel = df1[['timestamp', 'power', 'instant_power', 'cadence']]
+    df2_sel = df2[['timestamp', 'avg_heartrate']]
+
+    df_merged = pd.merge(df1_sel, df2_sel, on='timestamp', how='inner')
+
+    df_merged.to_csv(outF, index=False)
+
+
 def main():
-    inp = input('(1) hearthrate, (2) powermeter: ')
+    inpF = '../../dati/cerberus/balocco/20250614/run1/pwmR1.csv'
+    inpF2 = '../../dati/cerberus/balocco/20250614/run1/hearthrateR1.csv'
+    outF = '../../dati/cerberus/balocco/20250614/run1/run1.csv'
+
+    inp = input('(1) hearthrate, (2) powermeter, (3) unifica: ')
+
     if inp == '1':
-        hearthrate()
+        hearthrate(inpF, outF)
     
     elif inp == '2':
-        powermeter()
+        powermeter(inpF, outF)
+    
+    elif inp == '3':
+        unifica(inpF, inpF2, outF)
     
 
 main()
