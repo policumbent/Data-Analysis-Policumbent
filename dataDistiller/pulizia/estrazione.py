b22a9fccfb513b6641e3b54f15a1be90f9e03987
Mario <mr.tarantino02@gmail.com>
2025-09-13 21:16:45 +0200

    aggiunti pattern per bob e heartRate

--- dataDistiller/pulizia/estrazione.py
+++ dataDistiller/pulizia/estrazione.py
@@ -27,58 +27,74 @@ tk_battery = re.compile(r"TelekBattery:\s*(\d+)\s*%")
 # RxShifting
 rxShifting = re.compile(r"RxShifting:\s*(\d+)\s")
 
+# HeartRate
+heartRate  = re.compile(r'HeartRate:\s*(\d+)\s*bpm')
 
-pathI = '../../dati/Cerberus/stupinigi/20250906/rowdata/gns/txt_file'
-pathO = '../../dati/Cerberus/stupinigi/20250906/rowdata/gns/csv_file'
+# Bob Cadence and poweer
+power = re.compile(r'SrmPower:\s*([\d\.]+)\s*W')
+cadence = re.compile(r'SrmCadence:\s*([\d\.]+)\s*rpm')
+
+
+pathI = '../../dati/Cerberus/balocco/20250913/rowdata'
+pathO = '../../dati/Cerberus/balocco/20250913'
 
 for nf in os.listdir(pathI):
-    print(nf)
-    pathFile = os.path.join(pathI, nf)
-
-    with open(pathFile, 'r') as inF:
-        dati = list()
-        time = None
-        dati_i = dict()
-        
-        for line in inF:
-            timeMatch = time_pattern.search(line)
-            if timeMatch:
-                time = timeMatch.group(1)
-                dati.append(dati_i)
-                dati_i = dict()
-                dati_i = {'timestamp': time}
-
-            speed_match = speed_pattern.search(line)
-            distance_match = distance_pattern.search(line)
-            raw_speed_match = raw_speed_pattern.search(line)
-            raw_distance_match = raw_distance_pattern.search(line)
-            lat_match = lat_pattern.search(line)
-            lon_match = lon_pattern.search(line)
-            gear_match = gear_pattern.search(line)
-            tkBattery_match = tk_battery.search(line)
-            rxShiftit = rxShifting.search(line)
+    n,e = os.path.splitext(nf)
+    if e == '.txt':
+        pathFile = os.path.join(pathI, nf)
+        with open(pathFile, 'r') as inF:
+            dati = list()
+            time = None
+            dati_i = dict()
             
-            if speed_match:
-                dati_i['speed'] = float(speed_match.group(1))
-            if distance_match:
-                dati_i['distance'] = int(distance_match.group(1))
-            if raw_speed_match:
-                dati_i['raw_speed'] = float(raw_speed_match.group(1))
-            if raw_distance_match:
-                dati_i['raw_distance'] = int(raw_distance_match.group(1))
-            if lat_match:
-                dati_i['latitude'] = float(lat_match.group(1))
-            if lon_match:
-                dati_i['longitude'] = float(lon_match.group(1))
-            if gear_match:
-                dati_i['gear'] = int(gear_match.group(1))
-            if tkBattery_match:
-                dati_i['tkBattery'] = float(tkBattery_match.group(1))
-            if rxShiftit:
-                dati_i['rxShifting'] = int(rxShiftit.group(1))
+            for line in inF:
+                timeMatch = time_pattern.search(line)
+                if timeMatch:
+                    time = timeMatch.group(1)
+                    dati.append(dati_i)
+                    dati_i = dict()
+                    dati_i = {'timestamp': time}
+
+                speed_match = speed_pattern.search(line)
+                distance_match = distance_pattern.search(line)
+                raw_speed_match = raw_speed_pattern.search(line)
+                raw_distance_match = raw_distance_pattern.search(line)
+                lat_match = lat_pattern.search(line)
+                lon_match = lon_pattern.search(line)
+                gear_match = gear_pattern.search(line)
+                tkBattery_match = tk_battery.search(line)
+                rxShiftit = rxShifting.search(line)
+                heartRateM = heartRate.search(line)
+                poweM = power.search(line)
+                cadenceM = cadence.search(line)
+                
+                if speed_match:
+                    dati_i['speed'] = float(speed_match.group(1))
+                if distance_match:
+                    dati_i['distance'] = int(distance_match.group(1))
+                if raw_speed_match:
+                    dati_i['raw_speed'] = float(raw_speed_match.group(1))
+                if raw_distance_match:
+                    dati_i['raw_distance'] = int(raw_distance_match.group(1))
+                if lat_match:
+                    dati_i['latitude'] = float(lat_match.group(1))
+                if lon_match:
+                    dati_i['longitude'] = float(lon_match.group(1))
+                if gear_match:
+                    dati_i['gear'] = int(gear_match.group(1))
+                if tkBattery_match:
+                    dati_i['tkBattery'] = float(tkBattery_match.group(1))
+                if rxShiftit:
+                    dati_i['rxShifting'] = int(rxShiftit.group(1))
+                if heartRateM:
+                    dati_i['heartRate'] = int(heartRateM.group(1))
+                if cadenceM:
+                    dati_i['cadence'] = float(cadenceM.group(1))
+                if poweM:
+                    dati_i['power'] = float(poweM.group(1))
+            
+            df = pd.DataFrame(dati)
+            df = df.fillna(0)
+            nff, _ = os.path.splitext(nf) 
+            df.to_csv(os.path.join(pathO, nff + '.csv'), index=False)
         
-        df = pd.DataFrame(dati)
-        df = df.fillna(0)
-        nff, _ = os.path.splitext(nf) 
-        df.to_csv(os.path.join(pathO, nff + '.csv'), index=False)
-    
